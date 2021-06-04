20860aebdd29e5b29736b0cb9cf8d5e89d8acce9
ash001 <muradyan-ashot@mail.ru>
2021-06-04 12:26:47 +0400

    created Header Navbar & Profile components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/505px-Apple_logo_black.svg.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          alt=""
+        />{" "}
+      </div>
+      <div>avatar + description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
